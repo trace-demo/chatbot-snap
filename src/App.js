82dffc015f101bed12757e590a87acfbb26140ad
polarpolarbear <2788448286@qq.com>
2020-06-20 11:44:25 -0400

    add chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import ChatBot from 'react-simple-chatbot';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const steps = [
+  {
+    id: '0',
+    message: 'Welcome to react chatbot!',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'Bye!',
+    end: true,
+  },
+];
+
+
+class App extends React.Component {
+
+  render(){
+    return(
+      <div className="App">
+        <ChatBot steps={steps} />
+      </div>
+    )
+  }
+
 }
 
+
+
 export default App;
+ 
